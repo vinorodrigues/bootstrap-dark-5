a92f1931f2577da26b52e4154f78a2ca4463e7bc
Vino Rodrigues <github@m.vinorodrigues.com>
2021-01-20 22:56:37 +1100

    wip-2021-01-20

--- build/postcss.config.js
+++ build/postcss.config.js
@@ -2,13 +2,6 @@
 
 module.exports = ctx => {
   return {
-    map: ctx.file.dirname.includes('examples') ?
-      false :
-      {
-        inline: false,
-        annotation: true,
-        sourcesContent: true
-      },
     plugins: {
       autoprefixer: {
         cascade: false
