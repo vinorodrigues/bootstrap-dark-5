cc2c2f9786edfd2c5e4e17b36d431cde56af4d0b
Vino Rodrigues <366673+vinorodrigues@users.noreply.github.com>
2021-02-01 14:38:23 +1100

    adding eslint to the project

---
+++ js/src/darkmode.js
@@ -0,0 +1,7 @@
+/**
+ * ------------------------------------------------------------------------------------------------
+ * Bootstrap-Dark (v0.0.0-unreleased): darkmode.js
+ * Licensed under MIT (https://github.com/vinorodrigues/bootstrap-dark-5/blob/main/LICENSE.md)
+ * ------------------------------------------------------------------------------------------------
+ */
+
