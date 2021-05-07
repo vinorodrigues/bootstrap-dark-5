bcf91193a1408dacfce1572e25ff4ed9fb761729
Vino Rodrigues <github@m.vinorodrigues.com>
2021-05-07 20:14:37 +1000

    1.0.0 release

--- dist/js/darkmode.js
+++ dist/js/darkmode.js
@@ -1,5 +1,5 @@
 /*!
- * Bootstrap-Dark-5 v0.1.3 (https://vinorodrigues.github.io/bootstrap-dark-5/)
+ * Bootstrap-Dark-5 v1.0.0 (https://vinorodrigues.github.io/bootstrap-dark-5/)
  * Copyright 2021 Vino Rodrigues
  * Licensed under MIT (https://github.com/vinorodrigues/bootstrap-dark-5/blob/main/LICENSE.md)
  */
