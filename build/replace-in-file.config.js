7b68f74a6723d315d05514b4f91bd3f2a6c5961b
Vino Rodrigues <github@m.vinorodrigues.com>
2021-08-05 23:58:47 +1000

    align to BS 5.1 release

--- build/replace-in-file.config.js
+++ build/replace-in-file.config.js
@@ -1,9 +1,17 @@
 'use strict';
 
 const pkg = require('../package.json');
+const bsd_ver = pkg.version;
+const bs_ver = pkg.dependencies.bootstrap.replace(/[^\d.-]/g, '');
 
 module.exports = {
   files: 'dist/css/*.css',
-  from: '(https://vinorodrigues.github.io/bootstrap-dark-5/)',
-  to: `v${pkg.version} (https://vinorodrigues.github.io/bootstrap-dark-5/)`
+  from: [
+    '(https://vinorodrigues.github.io/bootstrap-dark-5/)',
+    'Bootstrap v5.x.x'
+  ],
+  to: [
+    `v${bsd_ver} (https://vinorodrigues.github.io/bootstrap-dark-5/)`,
+    `Bootstrap v${bs_ver}`
+  ]
 }
